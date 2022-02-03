23363cd0629e310a7af14da577fa206f1bb0bd13
Deriks Karlay Dias Costa <karlaycosta@gmail.com>
2022-02-02 21:11:08 -0300

    02/02/2022

--- bin/main.dart
+++ bin/main.dart
@@ -7,6 +7,7 @@ import 'package:shelf_cors_headers/shelf_cors_headers.dart';
 import 'package:shelf_router/shelf_router.dart';
 import 'package:shelf_static/shelf_static.dart';
 import 'package:sqflite_common_ffi/sqflite_ffi.dart';
+import 'package:sqflite_common/sqlite_api.dart';
 import 'package:path/path.dart' as path;
 import 'models/alimentos.dart';
 
@@ -15,22 +16,32 @@ void main() async {
   const port = 8080;
   final ip = InternetAddress.anyIPv4;
 
+  sqfliteFfiInit();
+  final caminho = path.join(path.current, 'evo.db');
+  final db = await databaseFactoryFfi.openDatabase(caminho);
+  print(db.isOpen);
+
   final overrideHeaders = {
     ACCESS_CONTROL_ALLOW_ORIGIN: '*',
   };
 
   // INÍCIO DAS ROTAS
   final app = Router();
+
   app.get('/alimentos', (Request request) async {
     return Response.ok(
-      await getAlimentos(),
+      await getAlimentos(db),
       headers: {'content-type': 'application/json'},
     );
   });
 
   app.get('/alimentosfiltrados/<predicado>', (Request request, String predicado) async {
+    final pred = Uri.decodeFull(predicado);
+    if (pred.length < 3) {
+      return Response(204);
+    }
     return Response.ok(
-      await getAlimentosFiltrados(predicado),
+      await getAlimentosFiltrados(db, pred),
       headers: {'content-type': 'application/json'},
     );
   });
@@ -52,20 +63,12 @@ void main() async {
   print('Servidor rodando no $ip:$port');
 }
 
-Future<String> getAlimentos() async {
-  sqfliteFfiInit();
-  final caminho = path.join(path.current, 'evo.db');
-  final db = await databaseFactoryFfi.openDatabase(caminho);
+Future<String> getAlimentos(Database db) async {
   final res = await db.rawQuery('select * from alimentos');
-  await db.close();
   return jsonEncode(res);
 }
 
-Future<String> getAlimentosFiltrados(String predicado) async {
-  sqfliteFfiInit();
-  final caminho = path.join(path.current, 'evo.db');
-  final db = await databaseFactoryFfi.openDatabase(caminho);
+Future<String> getAlimentosFiltrados(Database db, String predicado) async {
   final res = await db.rawQuery('''SELECT * FROM alimentos WHERE replace(replace(replace(replace(replace(replace(replace(replace(replace(replace(replace(alimentos.nome, 'á','a'), 'ã','a'), 'â','a'), 'é','e'), 'ê','e'), 'í','i'),'ó','o') ,'õ','o') ,'ô','o'),'ú','u'), 'ç','c') LIKE '%$predicado%';''');
-  await db.close();
   return jsonEncode(res);
 }
