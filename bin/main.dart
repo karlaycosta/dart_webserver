de27b31baa5ad1be775548cf720047896511ed30
Deriks Karlay Dias Costa <karlaycosta@gmail.com>
2022-02-03 21:36:59 -0300

    03/02/2022

--- bin/main.dart
+++ bin/main.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 import 'dart:io';
 
+import 'package:diacritic/diacritic.dart';
 import 'package:shelf/shelf.dart';
 import 'package:shelf/shelf_io.dart';
 import 'package:shelf_cors_headers/shelf_cors_headers.dart';
@@ -12,14 +13,12 @@ import 'package:path/path.dart' as path;
 import 'models/alimentos.dart';
 
 void main() async {
-
   const port = 8080;
   final ip = InternetAddress.anyIPv4;
 
   sqfliteFfiInit();
   final caminho = path.join(path.current, 'evo.db');
   final db = await databaseFactoryFfi.openDatabase(caminho);
-  print(db.isOpen);
 
   final overrideHeaders = {
     ACCESS_CONTROL_ALLOW_ORIGIN: '*',
@@ -28,6 +27,14 @@ void main() async {
   // INÍCIO DAS ROTAS
   final app = Router();
 
+  app.get('/teste', (Request request) {
+    return Response(
+      200,
+      body: '[{"nome1": "Deriks Karlay"}, {"nome2": "Deriks Karlay"}]',
+      headers: {'content-type': 'application/json'},
+    );
+  });
+
   app.get('/alimentos', (Request request) async {
     return Response.ok(
       await getAlimentos(db),
@@ -35,7 +42,8 @@ void main() async {
     );
   });
 
-  app.get('/alimentosfiltrados/<predicado>', (Request request, String predicado) async {
+  app.get('/alimentosfiltrados/<predicado>',
+      (Request request, String predicado) async {
     final pred = Uri.decodeFull(predicado);
     if (pred.length < 3) {
       return Response(204);
@@ -45,7 +53,6 @@ void main() async {
       headers: {'content-type': 'application/json'},
     );
   });
-
   // FIM DAS ROTAS
 
   final handlerStatic = createStaticHandler(
@@ -64,11 +71,13 @@ void main() async {
 }
 
 Future<String> getAlimentos(Database db) async {
-  final res = await db.rawQuery('select * from alimentos');
+  final res = await db.rawQuery('select * from alimentos limit 1000');
   return jsonEncode(res);
 }
 
 Future<String> getAlimentosFiltrados(Database db, String predicado) async {
-  final res = await db.rawQuery('''SELECT * FROM alimentos WHERE replace(replace(replace(replace(replace(replace(replace(replace(replace(replace(replace(alimentos.nome, 'á','a'), 'ã','a'), 'â','a'), 'é','e'), 'ê','e'), 'í','i'),'ó','o') ,'õ','o') ,'ô','o'),'ú','u'), 'ç','c') LIKE '%$predicado%';''');
+  predicado = removeDiacritics(predicado);
+  final res = await db.rawQuery(
+      '''SELECT * FROM alimentos WHERE replace(replace(replace(replace(replace(replace(replace(replace(replace(replace(replace(alimentos.nome, 'á','a'), 'ã','a'), 'â','a'), 'é','e'), 'ê','e'), 'í','i'),'ó','o') ,'õ','o') ,'ô','o'),'ú','u'), 'ç','c') LIKE '%$predicado%';''');
   return jsonEncode(res);
 }
--- bin/teste.dart
+++ bin/teste.dart
@@ -1,18 +1,20 @@
 import 'dart:convert';
 
+import 'package:diacritic/diacritic.dart';
 import 'package:sqflite_common_ffi/sqflite_ffi.dart';
 import 'package:path/path.dart' as path;
 
 void main() async {
-  sqfliteFfiInit();
-  final caminho = path.join(path.current, 'evo.db');
-  final db = await databaseFactoryFfi.openDatabase(caminho);
-  final predicado = 'macarrao';
-  final res = await db.rawQuery(
-    '''SELECT * FROM alimentos WHERE replace(replace(replace(replace(replace(replace(replace(replace(replace(replace(replace(alimentos.nome, 'á','a'), 'ã','a'), 'â','a'), 'é','e'), 'ê','e'), 'í','i'),'ó','o') ,'õ','o') ,'ô','o'),'ú','u'), 'ç','c') LIKE '%$predicado%';''');
-  print(res.length);
-  // for (final item in res) {
-  //   print(item);
-  // }
-  await db.close();
+  print(removeDiacritics('maçã'));
+  // sqfliteFfiInit();
+  // final caminho = path.join(path.current, 'evo.db');
+  // final db = await databaseFactoryFfi.openDatabase(caminho);
+  // final predicado = 'macarrao';
+  // final res = await db.rawQuery(
+  //   '''SELECT * FROM alimentos WHERE replace(replace(replace(replace(replace(replace(replace(replace(replace(replace(replace(alimentos.nome, 'á','a'), 'ã','a'), 'â','a'), 'é','e'), 'ê','e'), 'í','i'),'ó','o') ,'õ','o') ,'ô','o'),'ú','u'), 'ç','c') LIKE '%$predicado%';''');
+  // print(res.length);
+  // // for (final item in res) {
+  // //   print(item);
+  // // }
+  // await db.close();
 }
